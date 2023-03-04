fff9f43b03ed2a10c946a6a64385af5332518f39
Hossein Abdollahi <abdollahi_hossein@yahoo.com>
2023-03-04 13:18:44 -0500

    minor change in logger.js

--- src/features/middleware/logger.js
+++ src/features/middleware/logger.js
@@ -2,6 +2,7 @@ function logger(req, res, next) {
     console.log(`${req.method} - ${req.path}`)
     const now = new Date()
     console.log(`${now.getFullYear()}-${now.getMonth()+1}-${now.getDate()}     ${now.getHours()}:${now.getMinutes()}:${now.getSeconds()}`)
+    console.log('--------------------------------')
     next()
 }
 
