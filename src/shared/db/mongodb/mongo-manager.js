3b510e649952ccd39f54f5628ad630ff810c8026
Hossein Abdollahi <abdollahi_hossein@yahoo.com>
2023-04-25 00:53:55 -0400

    Editing Readme file

--- src/shared/db/mongodb/mongo-manager.js
+++ src/shared/db/mongodb/mongo-manager.js
@@ -8,7 +8,7 @@ const openMongoConnection = () => {
   db.once('open', function callback () {
     console.log("connected to MongoDB")
   })
-  mongoose.connect('mongodb+srv://hosseinabdollahi:JYRoHRmQdArfgNjS@cluster0.6jaunsx.mongodb.net/Rocket-Genesis-Api?retryWrites=true&w=majority')
+  mongoose.connect(process.env.MONGO_URI)
 }
 
 mongoose.set('strictQuery', true) 
